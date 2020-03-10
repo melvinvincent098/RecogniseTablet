147d5ee3b076339c03a3956056342b93680a1cfe
Melvin <b6006976@my.shu.ac.uk>
2020-03-10 16:44:42 +0000

    Add project files.

---
+++ RecogniseTablet/RecogniseTablet/Helper/APIHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RecogniseTablet.Helper
+{
+    public static class APIHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://faceserviceapi.azurewebsites.net/api/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
